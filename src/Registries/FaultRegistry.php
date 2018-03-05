24b18d82df775d62df4bdbce2a6e843df59b05ec
anderson <anderson.forta@hotmail.com>
2018-03-05 14:54:37 -0300

    add fault registry

---
+++ src/Registries/FaultRegistry.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Convenia\Dominio\PayrollExport\Registries;
+
+use Convenia\Dominio\PayrollExport\Fields\Formats\FieldC;
+
+/**
+ * Class PayrollRegistry.
+ */
+class FaultRegistry extends Registry
+{
+    protected $length = 43;
+
+    /**
+     * @var array
+     */
+    protected $defaultFields = [
+        'fixed' => [
+            'format'       => FieldC::class,
+            'position'     => 1,
+            'length'       => 2,
+        ],
+        'faultDate' => [
+            'format'       => FieldC::class,
+            'position'     => 3,
+            'length'       => 8,
+        ],
+        'faultType' => [
+            'format'       => FieldC::class,
+            'position'     => 11,
+            'length'       => 1,
+        ],
+    ];
+}
