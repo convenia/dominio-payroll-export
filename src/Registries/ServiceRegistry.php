1e4e3d52f12e1e05ce40f5a702868e66e82e6fed
anderson <anderson.forta@hotmail.com>
2018-03-05 15:23:13 -0300

    add service registry

---
+++ src/Registries/ServiceRegistry.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Convenia\Dominio\PayrollExport\Registries;
+
+use Convenia\Dominio\PayrollExport\Fields\Formats\FieldC;
+
+/**
+ * Class PayrollRegistry.
+ */
+class ServiceRegistry extends Registry
+{
+    protected $length = 43;
+
+    /**
+     * @var array
+     */
+    protected $defaultFields = [
+        'fixed' => [
+            'format'       => FieldC::class,
+            'position'     => 1,
+            'length'       => 20,
+        ],
+        'serviceCode' => [
+            'format'       => FieldC::class,
+            'position'     => 3,
+            'length'       => 10,
+        ],
+        'rubricCode' => [
+            'format'       => FieldC::class,
+            'position'     => 13,
+            'length'       => 9,
+        ],
+        'value' => [
+            'format'       => FieldC::class,
+            'position'     => 22,
+            'length'       => 9,
+        ],
+    ];
+}
